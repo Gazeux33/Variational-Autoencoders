6f4d475f5533e1f4a7a4d6c42b562476b6cee23f
Theo <thoecastillo@yahoo.com>
2024-04-28 22:20:43 +0200

    plot results

--- VAE/train.py
+++ VAE/train.py
@@ -4,14 +4,13 @@ import setup_data, engine, auto_encoder_model, encoder_model, decoder_model, uti
 
 from torchvision import transforms
 
-train = True
+train = False
 saving = False
 
 NUM_EPOCHS = 5
-Z_DIM = 100
+Z_DIM = 10
 BATCH_SIZE = 32
 LEARNING_RATE = 0.01
-BETA = 100
 
 DATA_DIR = "../data"
 
@@ -27,14 +26,16 @@ train_dataloader, test_dataloader = setup_data.create_dataloaders(root=DATA_DIR,
                                                                   batch_size=BATCH_SIZE
                                                                   )
 
-encoder_model = encoder_model.EncoderModelV1(z_dim=Z_DIM).to(device)
+encoder_model = encoder_model.EncoderModelV2(z_dim=Z_DIM).to(device)
 decoder_model = decoder_model.DecoderModelV0(z_dim=Z_DIM).to(device)
 
 autoencoder = auto_encoder_model.VariationalAutoEncoderModelV0(encoder=encoder_model,
-                                                               decoder=decoder_model).to(device)
+                                                               decoder=decoder_model,
+                                                               kl_weight=100).to(device)
 
 optimizer = torch.optim.Adam(autoencoder.parameters(), lr=LEARNING_RATE)
-loss_fn = loss.VaeLossV0(beta=BETA)
+
+loss_fn = loss.VaeLossV0(beta=200)
 
 if __name__ == "__main__":
     results = engine.train(model=autoencoder,
@@ -51,3 +52,4 @@ if __name__ == "__main__":
                          target_dir="models",
                          model_name="05_going_modular_script_mode_tinyvgg_model.pth")
     utils.predict(autoencoder, test_dataloader)
+    utils.plot_results(results)
