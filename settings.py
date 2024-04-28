9b2611934ab6d91dd78afc3586a17e7a003f7191
Theo <thoecastillo@yahoo.com>
2024-04-28 14:53:59 +0200

    up

--- settings.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-ZDIM = 512
-EPOCHS = 5
-LEARNING_RATE = 0.001
-BATCH_SIZE = 32
-MODEL_PATH = Path("models")
-MODEL_NAME = "ae.pth"
-MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
